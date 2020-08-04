61ffc8e25f043f18f135b0019e3fe6a0d117b8a2
Stephan Kuschel <stephan.kuschel@gmail.com>
2020-08-04 13:49:20 +0200

    add `observe` and `observe_time`
    in order to add callback function in specific intervals.

--- generatorpipeline/__init__.py
+++ generatorpipeline/__init__.py
@@ -8,13 +8,13 @@ a data processing pipeline using python generators and the multiprocessing libra
 
 from .generatorpipeline import pipeline
 from .helper import isiterator
-from .streamfunctions import simplecache
+from .streamfunctions import simplecache, observe, observe_time
 from . import accumulators  # noqa
 
 
 __all__ = ['pipeline']
 __all__ += ['isiterator']
-__all__ += ['simplecache']
+__all__ += ['simplecache', 'observe', 'observe_time']
 
 from ._version import get_versions  # noqa
 __version__ = get_versions()['version']
--- generatorpipeline/streamfunctions.py
+++ generatorpipeline/streamfunctions.py
@@ -17,9 +17,10 @@
 #
 
 '''
-This module offers functions to help with stream manipulation.
+This module offers functions which are gernerally helpful when working with generators.
 '''
 
+import time
 from collections import deque
 from .helper import isiterator
 
@@ -44,3 +45,46 @@ def simplecache(gen, length=8):
         yield list(cache)
         # no need to remove the oldest element, because `maxlen`
         # was specified when the deque was initialized.
+
+
+def observe(gen, *funcs, interval=1):
+    '''
+    calls all functions in `funcs` for every element in the generator. The return value of those
+    functions will be discarded.
+    The functions MUST NOT change the elements in the generator.
+
+    kwargs
+    ------
+      interval=1: int
+        using `interval=n` will call all functions on every `n`th value from the
+        generator.
+    '''
+    interval = int(interval)
+    for i, el in enumerate(gen):
+        if not i % interval:
+            for f in funcs:
+                f(el)
+        yield el
+
+
+def observe_time(gen, *funcs, interval=0.5):
+    '''
+    calls all functions in `funcs`. The return value of those
+    functions will be discarded.
+    The functions MUST NOT change the elements in the generator.
+
+    kwargs
+    ------
+      interval=0.5: float, time in seconds
+        using `interval=n` will attempt to call all functions once every n
+        seconds.
+    '''
+    interval = int(float(interval)*1e9)
+    tlast = 0
+    for el in gen:
+        t = time.time_ns()
+        if t - tlast > interval:
+            tlast = t
+            for f in funcs:
+                f(el)
+        yield el
